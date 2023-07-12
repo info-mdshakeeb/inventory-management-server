aaa401c385330eba3720eadc36357815d2392fa7
itzshakeeb <itzshakeeb@gmail.com>
2023-07-12 14:05:11 +0600

    added getProduct route

--- controller/Product.controller.js
+++ controller/Product.controller.js
@@ -1,3 +1,4 @@
+const ApiErrors = require('../errors/ApiErrors');
 const userServices = require('../services/product.service');
 const catchAsync = require('../utils/catchAsync');
 const sandResponse = require('../utils/sandResponse');
@@ -8,6 +9,19 @@ module.exports.addProduct = catchAsync(async (req, res) => {
     success: true,
     statusCode: 200,
     message: 'Product create successfully',
+    data: data,
+  });
+});
+
+module.exports.getProducts = catchAsync(async (req, res) => {
+  const data = await userServices.getUsers(req.body);
+  if (!data.length) {
+    throw new ApiErrors(404, 'product not found');
+  }
+  sandResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Product get successfully',
     data: data,
   });
 });
--- services/product.service.js
+++ services/product.service.js
@@ -4,6 +4,15 @@ const Product = require('../model/product.modal');
 module.exports.createUser = async (product) => {
   const id = await generateUserId();
   product.id = id;
+  if (product.quantity === 0) {
+    product.status = 'out-of-stock';
+  }
   const result = await Product.create(product);
+  if (!result) throw new Error('product is not created');
+  return result;
+};
+
+module.exports.getUsers = async () => {
+  const result = Product.find({});
   return result;
 };
